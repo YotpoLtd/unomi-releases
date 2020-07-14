cf3bd584451e5b9d01ea5ebcbfa9669d08f91307
Liran Yogev <23477645+lyogev@users.noreply.github.com>
2020-07-14 12:14:00 +0300

    Update buildAndDeployUnomiFromTag.sh

--- buildAndDeployUnomiFromTag.sh
+++ buildAndDeployUnomiFromTag.sh
@@ -2,5 +2,4 @@
 set -ev
 
 cp .travis.settings.xml $HOME/.m2/settings.xml
-mvn -B -DaltDeploymentRepository=snapshots::default::https://yotpo.jfrog.io/artifactory/maven
- -Dorg.slf4j.simpleLogger.log.org.apache.maven.cli.transfer.Slf4jMavenTransferListener=warn clean install -DskipTests -Drat.skip=true deploy
+mvn -B -Dorg.slf4j.simpleLogger.log.org.apache.maven.cli.transfer.Slf4jMavenTransferListener=warn clean install deploy -DskipTests -Drat.skip=true -DaltDeploymentRepository=snapshots::default::https://yotpo.jfrog.io/artifactory/maven
