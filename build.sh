28f3fa8a9ed92fb55e3d784b70305f73f02c7f3b
liatiusim <liusim@yotpo.com>
2020-08-05 17:48:38 +0300

    Use snapshot

--- build.sh
+++ build.sh
@@ -3,4 +3,4 @@ set -ev
 
 mvn -B -Dorg.slf4j.simpleLogger.log.org.apache.maven.cli.transfer.Slf4jMavenTransferListener=warn clean install -Drat.skip=true --update-snapshots
 cd itests
-mvn -Dit.test=org.apache.unomi.itests.AllITs, verify
+mvn -Dit.test=org.apache.unomi.itests.AllITs, verify --update-snapshots
