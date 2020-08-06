9500fe521b988c7fc6fdbf87b92f958579d827af
liatiusim <liusim@yotpo.com>
2020-08-06 19:48:40 +0300

    Add travis tests

---
+++ buildAndRunTests.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+set -ev
+
+mvn -B -Dorg.slf4j.simpleLogger.log.org.apache.maven.cli.transfer.Slf4jMavenTransferListener=warn clean install -Drat.skip=true --update-snapshots
+pushd itests
+mvn -Dit.test=org.apache.unomi.itests.AllITs, verify --update-snapshots
+popd
