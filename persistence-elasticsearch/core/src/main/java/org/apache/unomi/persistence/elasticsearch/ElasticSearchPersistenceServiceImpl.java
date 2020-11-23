d414867b6647fb28cee158c7371be2bcd6b7b26d
Gilad Weinbach <gweinbach@yotpo.com>
2020-11-23 09:23:43 +0200

    log time took

--- persistence-elasticsearch/core/src/main/java/org/apache/unomi/persistence/elasticsearch/ElasticSearchPersistenceServiceImpl.java
+++ persistence-elasticsearch/core/src/main/java/org/apache/unomi/persistence/elasticsearch/ElasticSearchPersistenceServiceImpl.java
@@ -924,7 +924,7 @@ public class ElasticSearchPersistenceServiceImpl implements PersistenceService, 
                 });
 
                 BulkResponse bulkResponse = client.bulk(bulkRequest, requestOptions);
-                logger.info("{} profiles updated with bulk segment in {}ms. timeTookByElastic={}", bulkRequest.numberOfActions(), System.currentTimeMillis() - batchRequestStartTime, bulkResponse.getIngestTookInMillis());
+                logger.info("{} profiles updated with bulk segment in {}ms. timeTookByElastic={}", bulkRequest.numberOfActions(), System.currentTimeMillis() - batchRequestStartTime, bulkResponse.getTook());
 
                 if (errorHandlerCallback != null){
                     List<String> failedIds = new ArrayList<>();
